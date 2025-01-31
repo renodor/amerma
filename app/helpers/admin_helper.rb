80eb0d40ea57307997832a9ce5b5ee198cc73d25
renodor <renaud.dor@gmail.com>
2025-01-31 10:53:23 +0100

    Improve design

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,7 +3,7 @@ module AdminHelper
     link_to(
       text,
       path,
-      class: "px-3 py-2 rounded text-gray-300 transition-colors hover:text-white hover:bg-slate-800 data-[active='true']:text-white data-[active='true']:bg-slate-800",
+      class: "px-3 py-2 text-gray-300 transition-colors hover:text-white hover:bg-gray-700 data-[active='true']:text-white data-[active='true']:bg-gray-700",
       data: { active: path[1..] == params[:controller] }
       )
   end
