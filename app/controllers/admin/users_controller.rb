d153ffbe6972430e5dc505dd3236fd59ed780ef5
renodor <renaud.dor@gmail.com>
2025-02-06 08:36:27 +0100

    Add update admin password feature

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,27 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.all
   end
+
+  def edit
+    redirect_to admin_users_path unless current_user.id == params[:id].to_i
+  end
+
+  def update
+    redirect_to admin_users_path and return unless current_user.id == params[:id].to_i
+
+    if current_user.update_with_password(user_params)
+      # Sign in the user by passing validation in case their password changed
+      bypass_sign_in(current_user)
+      flash[:success] = t("password_updated")
+    else
+      flash[:error] = t("update_password_error")
+      redirect_to edit_admin_user_path(current_user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
 end
