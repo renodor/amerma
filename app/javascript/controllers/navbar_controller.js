a43d9146038e27db75b5ffc5f141d91e3464d3f6
renodor <renaud.dor@gmail.com>
2025-02-16 12:19:19 +0100

    Harmonize design

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  openMobileNav() {
-    this.element.dataset.open = true
-  }
-
-  closeMobileNav() {
-    this.element.dataset.open = false
+  toggleMobileNav() {
+    this.element.dataset.open = !(this.element.dataset.open === "true")
   }
 }
