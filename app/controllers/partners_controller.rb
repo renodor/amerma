55ada4076b70a075f71ae50bc93597df147bc65b
Renaud Dor <renaud.dor@gmail.com>
2026-04-24 08:02:08 +0200

    Only display partner categories with partners. Only display description if present

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,10 @@
 class PartnersController < ApplicationController
   def index
-    @partner_categories = PartnerCategory.ordered.includes(partners: [logo_attachment: :blob])
+    @partner_categories = PartnerCategory
+      .ordered
+      .where(id: Partner.select(:partner_category_id))
+      .includes(partners: [logo_attachment: :blob])
+
     @partners_text = Page.find_by(name: "partners")
                          &.container_blocks
                          &.find_by(location: "partners")
